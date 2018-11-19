4dbbf30ee9e2441e62d6cf5cadab10d533078e9c
Damon Shetty <damon_53@hotmail.com>
2018-11-19 16:46:44 +0000

    Add project files.

---
+++ Word Puzzle Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Word_Puzzle_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
